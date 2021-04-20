4ff2576e5d2366d5837488cb3fe3d2de3f01d2c7
03one02one <ehrkd1021@naver.com>
2021-04-20 13:59:19 +0900

    코딩테스트(2) - 음양더하기

---
+++ 음양 더하기.js
@@ -0,0 +1,14 @@
+function solution(absolutes, signs) {
+    var answer = 0;
+    for(let i=0; i < signs.length; i++) {
+        if(signs[i] == true) {
+            answer += absolutes[i];            
+        }else {
+            answer += absolutes[i] * -1;
+        }
+    }
+    return answer;
+}
+
+console.log(solution([4, 7, 12], [true, false, true]));
+console.log(solution([1, 2, 3], [false, false, true]));
