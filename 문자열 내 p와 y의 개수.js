e83f3e06f5f6a619c27b092ca502a3eb9b414035
03one02one <ehrkd1021@naver.com>
2021-04-22 08:28:58 +0900

    코딩테스트 - 문자열 내 p와 y의 개수

--- 문자열 내 p와 y의 개수.js
+++ 문자열 내 p와 y의 개수.js
@@ -1,13 +1,22 @@
-function solution(s) {
+function solution(s){
     var answer = true;
-    s.toLowerCase();
-    var p_result = s.match(/p/g);
-    var y_result = s.match(/y/g);
-    if(p_result != null && y_result != null) {
-        if(p_result.length != y_result.length) {
-            answer = false;
+    var p_cnt = 0;
+    var y_cnt = 0;
+    
+    for(var i = 0; i < s.length; i++) {
+        if(s[i] == 'p' || s[i] == 'P') {
+            p_cnt += 1;
+        }else if(s[i] == 'y' || s[i] == 'Y') {
+            y_cnt += 1;
         }
     }
+    
+    if(p_cnt != y_cnt) {
+        return answer = false;
+    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+
     return answer;
 }
 
