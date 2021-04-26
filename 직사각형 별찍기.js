5ac19cd4e38bcc5763db9fc26969ca4a1797fac9
03one02one <ehrkd1021@naver.com>
2021-04-26 20:28:11 +0900

    코딩테스트 - 직사각형 별찍기

--- 직사각형 별찍기.js
+++ 직사각형 별찍기.js
@@ -2,6 +2,11 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    console.log(a);
-    console.log(b);
+    var answer = '';
+    for(let i = 0; i < b; i++) {
+        answer += "*".repeat(a) + '\n';
+    }
+    console.log(answer);
+    // console.log(a);
+    // console.log(b);
 });
