5410e098171764c6fed723ec9e819150754651ef
03one02one <ehrkd1021@naver.com>
2021-04-27 12:02:22 +0900

    코딩테스트 - 최대값과 최솟값(2)

--- 최대값과 최솟값.js
+++ 최대값과 최솟값.js
@@ -1,4 +1,11 @@
 function solution(s) {
     var answer = '';
+    var arr = s.split(' ');
+    
+    var max = Math.max.apply(null, arr);
+    var min = Math.min.apply(null, arr);
+    
+    answer = min + ' ' + max;
+    
     return answer;
 }
